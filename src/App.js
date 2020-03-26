48e9cf570cb33e6cf5faaa0a1c26462119bdf331
Olawale Sunmonu-Adedeji <olawalesunmonu-adedeji@Olawales-MacBook-Pro.local>
2020-03-27 00:25:13 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import OrderBook from './OrderBook';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <h2>Crypto Order Book</h2>
+      <OrderBook/>
     </div>
-  );
+  )
 }
 
 export default App;
